41a220b79c86131902cdd2a33f8675d1f994a3f9
tom willaert <tom willaert>
2016-06-19 15:03:51 +0200

    Added all unity files

---
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+    private NavMeshAgent enemyNavMesh;
+    private Transform myTransform;
+    private RaycastHit hit;
+    private Collider[] hitColliders;
+    private Vector3 lastSeenPosition = new Vector3();
+
+    public float checkRadius;
+    public LayerMask checkLayer;
+    
+    public float checkRate;
+    private float nextCheck;
+
+	void OnEnable() 
+	{
+		SetInitialReferences();
+	}
+
+	void OnDisable() 
+	{
+
+	}
+	
+	void Update () 
+	{
+        DetectEnemy();
+	}
+
+	void SetInitialReferences() 
+	{
+        enemyNavMesh = gameObject.GetComponent<NavMeshAgent>();
+        myTransform = gameObject.transform;
+	}
+
+    void DetectEnemy()
+    {
+        if (Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+
+            hitColliders = Physics.OverlapSphere(myTransform.position, checkRadius, checkLayer);
+
+            if (hitColliders.Length > 0)
+            {
+                MoveTowardsEnemy(hitColliders);
+            }
+            else
+            {
+                Wander();
+            }
+        }
+    }
+
+    void MoveTowardsEnemy(Collider[] hitColliders)
+    {
+        enemyNavMesh.SetDestination(hitColliders[0].transform.position);
+        lastSeenPosition = hitColliders[0].transform.position;
+    }
+
+    void Wander()
+    {
+        Vector3 randomWanderLocation = new Vector3();
+
+        randomWanderLocation = Random.insideUnitSphere * 50 + lastSeenPosition;
+        enemyNavMesh.SetDestination(randomWanderLocation);
+    }
+}
